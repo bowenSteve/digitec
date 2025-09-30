373e5b47730cfd49fdb0d7a259c3d6c241dd5d4e
Mark Castro <markcastorm@gmail.com>
2025-09-30 19:01:26 +0300

    adds solutions

--- client/src/Pages/Solutions/Solutions.js
+++ client/src/Pages/Solutions/Solutions.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../../Components/Navbar'
 import Footer from '../../Components/Footer'
 import SolutionsHero from './components/SolutionsHero'
+import SolutionsCont from './components/SolutionsCont'
 
 function Solutions() {
   return (
     <div>
         <Navbar/>
         <SolutionsHero/>
+        <SolutionsCont/>
         <Footer/>
       
     </div>
